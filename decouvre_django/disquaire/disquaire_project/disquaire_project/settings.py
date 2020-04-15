1146594f3dd2949d6d05dd3cc0576df5dc36a0bc
standemdem <37410638+standemdem@users.noreply.github.com>
2020-04-15 15:28:56 +0200

    modified base settings

--- decouvre_django/disquaire/disquaire_project/disquaire_project/settings.py
+++ decouvre_django/disquaire/disquaire_project/disquaire_project/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'disquaire_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql', # on utilise l'adaptateur postgresql
+        'NAME': 'disquaire', # le nom de notre base de donnees creee precedemment
+        'USER': 'stanislas', # attention : remplacez par votre nom d'utilisateur
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '5432',
     }
 }
 
@@ -103,9 +107,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'fr'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Europe/Paris'
 
 USE_I18N = True
 
